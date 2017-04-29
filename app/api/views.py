ddd573fcff9c4928b4515aecade81dfed98a93b5
Patrick <patrick.concepcion33@gmail.com>
2017-04-30 01:43:52 +0800

    Add an change password endpoint

--- app/api/views.py
+++ app/api/views.py
@@ -21,39 +21,60 @@ class UserViewSet(model_set):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
 
+    # FOR RESPONSE MESSAGES AND STATUS
+    r_text = 'OK'
+    r_status = status.HTTP_200_OK
 
-    @list_route(methods=['post','get'])
+    @list_route(methods=['post'])
     def user_signup(self, request):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            serializer = serializer.validated_data
+            r_text = serializer.validated_data
+            r_status = status.HTTP_200_OK
         else:
-            serializer = serializer.errors
+            r_text = serializer.errors
+            r_status = status.HTTP_400_BAD_REQUEST
 
-        return Response(serializer)
+        return Response(r_text, status=r_status)
 
 
     @list_route(methods=['post'])
     def user_login(self, request):
         serializer = UserLoginSerializer(data=request.data)
-        r_text = 'OK'
-        r_status = status.HTTP_200_OK
-
         if serializer.is_valid():
             user = authenticate(
                 username=request.data.get('email'),
                 password=request.data.get('password')
             )
             if user and user.is_active:
-                pass
+                r_text = serializer.validated_data
+                r_status = status.HTTP_200_OK              
             else:
                 r_text = 'Invalid Username/Password'
                 r_status = status.HTTP_401_UNAUTHORIZED
 
         else:
-            r_text = 'Invalid Username/Password'
-            r_status = status.HTTP_401_UNAUTHORIZED
+            r_text = serializer.errors
+            r_status = status.HTTP_400_BAD_REQUEST
+
+
+        return Response(r_text, status=r_status)
+
+
+    @list_route(methods=['patch'])
+    def change_password(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            request.user.set_password(
+                request.data['password']
+            )
+            request.user.save()
+            r_text = 'Successfully changed password.'
+            r_status = status.HTTP_200_OK
+        else:
+            r_text = serializer.errors 
+            r_status = status.HTTP_400_BAD_REQUEST
 
 
         return Response(r_text, status=r_status)
